123b61c3e0af8675e7fcb4d9413ed5e2e278ba17
ecambronerosaba <esteban@enya.ai>
2022-02-01 18:46:18 -0800

    Added Input Box

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import GameRow from "./GameRow";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { idx: 0, passed: false };
+    this.state = { idx: 0, passed: false, pot_idx: 0 };
     this.gameRowRefs = [
       React.createRef(),
       React.createRef(),
@@ -20,7 +20,27 @@ class App extends React.Component {
   onClick = () => {
     this.setState({ idx: this.state.idx + 1, passed: false });
     var inputElements = document.getElementsByTagName("input");
-    for (var i = 0; i < inputElements.length; i++) {
+    for (var i = 1; i < inputElements.length; i++) {
+      if (inputElements[i].type === "text") {
+        inputElements[i].value = "";
+      }
+    }
+
+    for (var i = 0; i < this.gameRowRefs.length; i++) {
+      this.gameRowRefs[i].current.reset();
+    }
+  };
+
+  onChange = (e) => {
+    this.setState({ pot_idx: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    if (e.keyCode === 13) {
+      this.setState({ idx: this.state.pot_idx });
+    }
+    var inputElements = document.getElementsByTagName("input");
+    for (var i = 1; i < inputElements.length; i++) {
       if (inputElements[i].type === "text") {
         inputElements[i].value = "";
       }
@@ -50,7 +70,16 @@ class App extends React.Component {
     }
     return (
       <div className="">
-        <div className="title">{`WORDLE #${this.state.idx}`}</div>
+        <div className="title">
+          <span>WORDLE #</span>
+          <span>
+            <input
+              onChange={this.onChange}
+              onKeyUp={this.onSubmit}
+              defaultValue={this.state.idx}
+            ></input>
+          </span>
+        </div>
         {gameRows}
         {this.state.passed ? (
           <button onClick={this.onClick}> Next</button>
